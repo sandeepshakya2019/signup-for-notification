8a7cdb1186e3868f2c7f31528d3e19fee80e5aa3
Sandeep Kumar <51978973+sandeepshakya2019@users.noreply.github.com>
2020-05-19 00:01:44 +0530

    Add files via upload

---
+++ mail.js
@@ -0,0 +1,65 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const request = require('request');
+const https = require('https');
+
+const app = express();
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.get("/",function(req,res){
+	res.sendFile(__dirname+"/signup.html");
+});
+app.post("/",function(req,res){
+	const name = req.body.name;
+	const email = req.body.email;
+	//console.log(name,email);
+	const data = {
+		members:[
+			{
+			email_address: email,
+			status:"subscribed",
+			merge_fields:{
+				FNAME:name
+			}
+		}]
+	};
+
+	const jsonData = JSON.stringify(data);
+	const url = 'https://us18.api.mailchimp.com/3.0/lists/757c40c579';
+	const options = {
+		method : "POST",
+		auth : "sandeep:849751528e1c10b01fd4d42c4562d12c-us18"
+	}
+	//const
+
+	const request = https.request(url,options,function(response){
+		if(response.statusCode === 200){
+			res.sendFile(__dirname+"/success.html");
+		}else{
+			res.sendFile(__dirname+"/failer.html");
+		}
+		response.on("data",function(data){
+			console.log(JSON.parse(data));
+		})
+		
+	})
+	//quest.write(jsonData);
+	request.end();
+});
+
+app.post("/failer",function(req,res){
+	res.redirect("/");
+
+})
+//{"name":"Freddie'\''s Favorite Hats","contact":{"company":"Mailchimp","address1":"675 Ponce De Leon Ave NE","address2":"Suite 5000","city":"Atlanta","state":"GA","zip":"30308","country":"US","phone":""},"permission_reminder":"You'\''re receiving this email because you signed up for updates about Freddie'\''s newest hats.","campaign_defaults":{"from_name":"Freddie","from_email":"freddie@freddiehats.com","subject":"","language":"en"},"email_type_option":true}
+
+app.listen(3000,function(){
+	console.log("I am listening at the port 3000");
+})
+
+//Api key 
+//849751528e1c10b01fd4d42c4562d12c-us18
+
+//list id
+//757c40c579
---
+++ readme.txt
@@ -0,0 +1,3 @@
+First download the gitbash terminal and node.js (download from official sites) then proceed with the steps ehich is given in the steps folder
+and download the files signup.html,mail.js,success.js,failer,js
+make sure you have a public folder in which css files and image files are present
